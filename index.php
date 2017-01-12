bb7d057f05924560c0b37af59466624345b81973
kolqta <kolqta@abv.bg>
2017-01-12 16:40:35 +0200

    добавена кирилицация

--- index.php
+++ index.php
@@ -1,11 +1,7 @@
 <?php
 
-<<<<<<< HEAD
+header('Content-Type: text/html; charset=utf-8');
 // коментар
 echo "За да ми видиш примерен код виж директориите 'models' - 'controllers', Системата не мога да ти покажа но е писано на 'CodeIgniter'";
-=======
-// за проба
-echo "test - wyw wtora stypka ";
->>>>>>> ee60413fe051c962689199aa6da622306da19bd7
 
 ?>
