e4dc509729c4fe57c6bee3187ca4a1d28dd3dd4d
kolqta <kolqta@abv.bg>
2017-01-13 15:37:45 +0200

    redakciq
    redaktirano ot syrwyra

--- index.php
+++ index.php
@@ -4,4 +4,5 @@ header('Content-Type: text/html; charset=utf-8');
 // коментар
 echo "За да ми видиш примерен код виж директориите 'models' - 'controllers', Системата не мога да ти покажа но е писано на 'CodeIgniter'";
 
+//dobaweno ot syrwyra komentar
 ?>
